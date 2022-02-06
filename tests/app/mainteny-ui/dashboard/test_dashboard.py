f88ab2dc63493eda2eef3c255206b170876874c2
abani.maharana <abanimaharana1996@gmail.com>
2022-02-07 05:20:04 +0530

    Completed the UI Test assignment.

--- tests/app/mainteny-ui/dashboard/test_dashboard.py
+++ tests/app/mainteny-ui/dashboard/test_dashboard.py
@@ -1,10 +1,12 @@
 """DASHBOARD TEST MODULE """
+import pytest
 
 
+@pytest.mark.ui
+@pytest.mark.ui_smoke
 def test_dashboard(_user_login, frontend):
     """ Verify the pending orders in Dashboard."""
     expected_pending_orders = '55'
     frontend.py.wait(use_py=True).sleep(3)
     actual__pending_orders = frontend.dashboard_page.pending_orders.text().strip()
     assert expected_pending_orders == actual__pending_orders, "Pending orders didn't match with expectation."
-
